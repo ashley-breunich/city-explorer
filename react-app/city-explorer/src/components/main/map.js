4dd16b8a7acdc6c4930e80fd13f23b2624214f90
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2018-12-28 16:27:41 -0800

    got map toggle working

--- react-app/city-explorer/src/components/main/map.js
+++ react-app/city-explorer/src/components/main/map.js
@@ -4,13 +4,16 @@ const Map = props => {
 
   let googleMapSRC = `https://maps.googleapis.com/maps/api/staticmap?center=${props.location.latitude}%2c%20${props.location.longitude}&zoom=13&size=600x300&maptype=roadmap
   &key=AIzaSyDp0Caae9rkHUHwERAFzs6WN4_MuphTimk`;
-
-  return (
-    <React.Fragment>
-      <img id="map" src={googleMapSRC} alt="Map of search query" />
-      <h2 className="query-placeholder">Here are the results for {props.location.formatted_query}</h2>
-    </React.Fragment>
-  );
+  if(props.visibility === true) {
+    return <div></div>;
+  } else {
+    return (
+      <React.Fragment>
+        <img id="map" src={googleMapSRC} alt="Map of search query" />
+        <h2 className="query-placeholder">Here are the results for {props.location.formatted_query}</h2>
+      </React.Fragment>
+    );
+  }
 };
 
 export default Map;
